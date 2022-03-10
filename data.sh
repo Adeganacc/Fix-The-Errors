f5f19ddf696aefb0ac6ab233bfaf0882be029d30
Adeganacc <100861893+Adeganacc@users.noreply.github.com>
2022-03-10 09:40:38 -0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,13 +3,13 @@
 
 # Create a directory called new_data
 mkdir new_data
-
+cd new_data
 # Create 5 empty files in the new_data directory
 # Call the files data1, data2, data3, data4, and data5
 touch data1 data2 data3 data4 data5
 
 # Output 'author: <username>' to each data file
-echo 'author: ' $USER> data*
+echo 'author: ' $USER > data*
 
 # Append 'date: ' and the date in MM-DD-YYYY format to each data file
 echo 'date: ' date +$m-$d-$Y >> data*
